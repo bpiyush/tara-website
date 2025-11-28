129c6aeb020a6b03d0e17270a0043f20bd39c163
Piyush (Mab M1) <piyushnbagad11@gmail.com>
2025-11-28 14:52:36 +0000

    Minor

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Setting base to './' allows the app to be deployed to any subdirectory
-  // (like https://username.github.io/repo-name/) without absolute path issues.
-  base: './', 
-});
+  base: '/bpiyush/',   // MUST match repo name exactly
+})
