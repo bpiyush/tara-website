b86e0213c423bc32875dfa39f4e4e4f8e03c4c90
Piyush (Mab M1) <piyushnbagad11@gmail.com>
2026-04-24 15:50:41 +0100

    Minor: deleted old file

--- vite.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  base: './',   // MUST match repo name exactly
-})
